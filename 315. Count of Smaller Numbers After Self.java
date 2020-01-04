42f5989a5f00a2f6b6cf4ecd84f54ca985be9b56
LittleTiger <59035218+soccerplayer16@users.noreply.github.com>
2020-01-04 15:25:57 -0800

    Create 315. Count of Smaller Numbers After Self.java

---
+++ 315. Count of Smaller Numbers After Self.java
@@ -0,0 +1,62 @@
+# Technically this is O(N2) method in worse case (the input array is descending order from left to right, in which case, the BST is a linked list). In a random case, the amortized time complexity is O(NlogN).
+
+class Solution {
+
+	private class Node {
+		int val;
+		int preCount; // count how many numbers are smaller than this number
+		int dup; // count how many numbers are duplicates at this number
+		Node left, right;
+		private Node (int v, int pre) {
+			val = v;
+			preCount = pre; // initialization ==> how many numbers are smaller than this number (node)
+			dup = 1; // how many duplicates for this number
+			left = null;
+			right = null;
+		}
+	}
+
+	public List<Integer> countSmaller(int[] nums) {
+
+		if (nums == null || nums.length == 0) {
+			return new ArrayList<Integer>();
+		}
+
+		int n = nums.length;
+		Integer[] res = new Integer[n];
+
+		// initialization of the BST, use last number as root of the tree //
+		Node root = new Node(nums[n - 1], 0);
+		res[n - 1] = 0; // root.preCount
+
+		for (int i = n - 2; i >= 0; i--) {
+			Node node = new Node(nums[i], 0);
+			helper(res, node, root, 0, i); // traverse all the elements from right to left
+		}
+
+		return Arrays.asList(res);
+	}
+
+	private void helper(Integer[] res, Node node, Node root, int pre, int i) {
+		// 3 cases -> where to insert the new node //
+		if (node.val == root.val) {
+			root.dup ++;
+			res[i] = pre + root.preCount;
+		} else if (node.val < root.val) {
+			root.preCount ++;
+			if (root.left == null) {
+				root.left = node;
+				res[i] = pre;
+			} else {
+				helper(res, node, root.left, pre, i);
+			}
+		} else {
+			if (root.right == null) {
+				root.right = node;
+				res[i] = pre + root.preCount + root.dup;
+			} else {
+				helper(res, node, root.right, pre + root.preCount + root.dup, i);
+			}
+		}
+	}
+}
